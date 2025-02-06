e3ee9d09cc2296c8beeee717c6d0d8e3fdde8b11
Rishabh Rana <rishabh.rana@hungerbox.com>
2025-02-06 20:30:41 +0000

    better prompts and input

--- menu_mapping/helper_classes/llm_helper.py
+++ menu_mapping/helper_classes/llm_helper.py
@@ -36,29 +36,32 @@ class ItemFormatter:
 
     def format(self, item_name):
         prompt = """Given the name of a food item name (can be Indian), extract the different food items present in the name and reply with a ' | ' seperated string (Look at the example). Remove any sort of price or quantity from the said food item name for the name column.
-                    Example: 'Medium Pizza Non veg' becomes 'Pizza Non Veg' and so on.
+                    Example: 'Medium Pizza Non veg' becomes 'Pizza Non Veg', 'Sambar Idli' becomes 'Sambar | Idli', 'Veg Manchurian Noodle Combo' becomes 'Veg Manchurian | Noodles' and so on.
                     Also extract the quantity (in brackets) and price (in Rs) from the said food item name and reply with a ' | ' seperated string for the quantity_details column.
                     Keep definitive spellings for indian food items like 'pakora', 'bhaji', 'bajji', 'chapati', 'paratha', 'idli', 'bhatura' and so on. Example correct 'Chapathi', 'Chappathi' to 'Chapati'. Correct 'Laccha' to 'Lachha'.
                     Don't get confused with 'bajji' and 'bajji' as both are different items.
                     Spell correct 'rool' to 'roll', 'Subji' to 'sabji' and so on.
                     Note: Things like 'parotta' should not get converted to 'paratha' and 'Kal Dosa' should remain as 'Kal Dosa'.
-                    Also get rid of unnecessary words like 'special' or 'spl' in the item name. Do not remove important things like 'non veg' or 'veg'
+                    Also get rid of unnecessary words like 'special', 'spl', 'mini', 'jumbo', 'large', 'medium', 'small' in the item name. Do not remove important things like 'non veg' or 'veg'
+                    Convert 'dosai' to 'dosa'.
 
                     You need to determine whether an item name is ambiguous or specific. A food item is considered ambiguous if its name is vague and not a specific food, such as when it only states unspecific descriptors.
-                    For example, names like 'Chocolate Desire Heart' or 'juice of the day' are ambiguous.
-                    Generic terms such as 'Combo', 'Dinner', 'Variety', 'Menu', 'Thali', 'Meal', and 'Lunch' are ambiguous because they don't specify any particular item. (Note: any item with 'thali' in its name is automatically considered ambiguous.) 
+                    For example, names like 'Chocolate Desire Heart', 'juice of the day' 'Paneer Snacks', 'full non veg chicken meal' are ambiguous food items. As in snacks we can hav any item.
+                    Generic terms such as 'Dinner', 'Variety', 'Menu', 'Thali', 'Meal', and 'Lunch' are ambiguous because they don't specify any particular item. (Note: any item with 'thali' in its name is automatically considered ambiguous.) 
+                    
                     A single word like 'sweet' or 'sabji' is ambiguous. On the other hand, an item is not ambiguous if it provides specific details about the item. 
-                    For instance, 'desire heart cake' or 'Juice' are considered specific. 
-                    Detailed names like 'idli vada combo', 'basanti dum pulao mutton kosha combo', 'Kaju Katli Sweet', 'aloo sabji', 'rice bowl', 'date and walnut cake' clearly indicate what the item is and therefore are not ambiguous. 
-                    Note: 'dry fruit milkshake', 'Mango Masti', 'Chocolate Drink', 'Hot Chocolate' are specific enough for this usecase and thus are not ambiguous.
+                    For instance, 'desire heart cake' or 'Juice' are considered specific.
+                    Not Ambigious Items -> 'idli vada combo', 'Veg Manchurian Noodle Combo' 'basanti dum pulao mutton kosha combo', 'Kaju Katli Sweet', 'aloo sabji', 'rice bowl', 'date and walnut cake'.
+                    Reason -> They clearly indicate what the item is and therefore are not ambiguous. 
+                    Note: 'dry fruit milkshake', 'Mango Masti', 'Chocolate Drink', 'Hot Chocolate', 'Butter Scotch Ice Cream Shake', 'Death By Chocolate' are specific enough for this usecase and thus are not ambiguous.
                     Note: single fruit and vegetable names are not ambiguous and are specific for this usecase.
-                    Note: Even though 'Tea' is a general term, it should be treated as a specific item, not an ambiguous one."
+                    Note: Even though 'Tea', 'Biscuit' is a general term, it should be treated as a specific item, not an ambiguous one."
 
                     Also tell if the item is a retail store food item (mrp) or a restaurant dish (non_mrp).
                     Note: items like 'samosa', 'pakora', 'muffin', 'bread', 'Veg sandwich', 'curd' , 'Lemon Juice' and so on are not MRP items and are not ambiguous.
-                    Formatted Name should have proper capitalization (start of each important word in capital) (look at the example below). 
+                    Formatted Name should have proper capitalization (start of each important word in capital) (look at the example below).
                     Note : 'Chicken Egg Biryani', 'Chicken paratha', 'Chicken Egg roll' are same single items while 'Chapati 3 Egg curry' should be separated like 'Chapati, Egg Curry'. 'Aloo Paratha - 1 No With Channa Masala - 60 Grm Curd' Should be separated like 'Aloo Paratha, Channa Masala, Curd'
-                    Items like 'fried rice chicken', 'usal pav', 'bread butter jam' and 'chicken fried rice' are single items.
+                    Items like 'fried rice chicken', 'usal pav', 'bread butter jam', 'oats apple jar', 'palak onion pakoda'and 'chicken fried rice' are single items.
                     Note: Brand names should are important and should not be removed like 'Amul', 'Domino's' etc.
                     Output should be in json format with double quotes and enclosed in ```json {} ```
                     Output 'name' field should not contain any commas. Remove 'add on'/'addon' from input if present.
@@ -77,6 +80,14 @@ class ItemFormatter:
                     "name": "glazed night snack",
                     "quantity_details": "glazed night snack",
                     "ambiguous": 1,
+                    "is_mrp": 0
+                    }```
+                    
+                    Input: 'veg manchurian noodle combo
+                    Output:```json{
+                    "name": "Veg Manchurian | Noodles",
+                    "quantity_details": "Veg Manchurian | Noodles",
+                    "ambiguous": 0,
                     "is_mrp": 0
                     }```
                     
