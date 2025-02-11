f3a291ea6168b0fbec5c154d7131252f20107831
Rishabh Rana <rishabh.rana@hungerbox.com>
2025-02-11 09:40:53 +0000

    remove temperature etc

--- menu_mapping/helper_classes/menu_mapper_helper.py
+++ menu_mapping/helper_classes/menu_mapper_helper.py
@@ -139,84 +139,91 @@ class MenuMapperAI:
             item = data['name']
             eval_prediction, eval_input = 'NULL', 'NULL'
             valid = True
+            
+            try:
 
-            item_data = ItemFormatter('models/gemini-2.0-flash').format(item)
-            print(f'Item Name: {item}, Formatted Name: {item_data['name']}, Ambiguous: {item_data['ambiguous']}, Is MRP: {item_data['is_mrp']}, Quantity Details: {item_data['quantity_details']}')
-            if item_data['ambiguous']:
-                print("Ambiguous item, skipping...")
-                valid = False
-                root_item_name = 'AMBIGUOUS ITEM DETECTED'
-            if item_data['is_mrp']:
-                print("MRP item, skipping...")
-                valid = False
-                root_item_name = 'MRP ITEM DETECTED'
+                item_data = ItemFormatter('models/gemini-2.0-flash').format(item)
+                print(f'Item Name: {item}, Formatted Name: {item_data['name']}, Ambiguous: {item_data['ambiguous']}, Is MRP: {item_data['is_mrp']}, Quantity Details: {item_data.get('quantity_details')}')
+                if item_data['ambiguous']:
+                    print("Ambiguous item, skipping...")
+                    valid = False
+                    root_item_name = 'AMBIGUOUS ITEM DETECTED'
+                if item_data['is_mrp']:
+                    print("MRP item, skipping...")
+                    valid = False
+                    root_item_name = 'MRP ITEM DETECTED'
 
-            if valid:
-                nodes, _, _, _, _ = self.get_filtered_nodes(item_data['name'])
-                for node in nodes:
-                    info = node.text.split(",")
-                    ranked_nodes[info[0]] = info[1]
-                if len(nodes) == 0:
-                    print("Reranker returned nothing !!!!!")
-                    eval_input = Evaluator('models/gemini-2.0-flash').item_evaluator(data['mv_name'], item)
-                    eval_input = bool(eval_input == 'YES')
-                    # ouput_text += f"{data['id']},{data['name']},{data['mv_id']},{data['mv_name']},'{eval_input}','NOT FOUND','NOT FOUND','NULL','0','NULL','NULL'\n"
-                    prediction = MenuMappingPrediction(menu_id=data['id'], menu_name=data['name'], master_menu_id=data['mv_id'], master_menu_name=data['mv_name'], corrected_menu_name=item_data['name'],
-                                                       eval_current=eval_input, predicted_menu_name='NOT FOUND', eval_prediction=None,
-                                                       response='', ranked_nodes=ranked_nodes, log_id=log_id, quantitative_menu_name=item_data['quantity_details'])
-                    prediction.save()
-                    continue
+                if valid:
+                    nodes, _, _, _, _ = self.get_filtered_nodes(item_data['name'])
+                    for node in nodes:
+                        info = node.text.split(",")
+                        ranked_nodes[info[0]] = info[1]
+                    if len(nodes) == 0:
+                        print("Reranker returned nothing !!!!!")
+                        eval_input = Evaluator('models/gemini-2.0-flash').item_evaluator(data['mv_name'], item)
+                        eval_input = bool(eval_input == 'YES')
+                        # ouput_text += f"{data['id']},{data['name']},{data['mv_id']},{data['mv_name']},'{eval_input}','NOT FOUND','NOT FOUND','NULL','0','NULL','NULL'\n"
+                        prediction = MenuMappingPrediction(menu_id=data['id'], menu_name=data['name'], master_menu_id=data['mv_id'], master_menu_name=data['mv_name'], corrected_menu_name=item_data['name'],
+                                                        eval_current=eval_input, predicted_menu_name='NOT FOUND', eval_prediction=None,
+                                                        response='', ranked_nodes=ranked_nodes, log_id=log_id, quantitative_menu_name=item_data.get('quantity_details'))
+                        prediction.save()
+                        continue
 
-                text = "ID,Food Item Name,Vector Score\n"
-                for node in nodes:
-                    text += f"{node.node.text},{node.score}\n"
+                    text = "ID,Food Item Name,Vector Score\n"
+                    for node in nodes:
+                        text += f"{node.node.text},{node.score}\n"
 
-                # preparing query engine on the filtered index
-                filtered_index = VectorStoreIndex.from_documents([Document(text=text)])
-                query_engine = filtered_index.as_query_engine(embeddings_enabled=True)
+                    # preparing query engine on the filtered index
+                    filtered_index = VectorStoreIndex.from_documents([Document(text=text)])
+                    query_engine = filtered_index.as_query_engine(embeddings_enabled=True)
 
-                if self.benchmark_on:
-                    with self.tru_recorder:
-                        response = query_engine.query(self.prompt + item_data['name'])
-                else:
-                    while RETRY_COUNT > 0:
-                        try:
+                    if self.benchmark_on:
+                        with self.tru_recorder:
                             response = query_engine.query(self.prompt + item_data['name'])
+                    else:
+                        while RETRY_COUNT > 0:
+                            try:
+                                response = query_engine.query(self.prompt + item_data['name'])
+                                break
+                            except Exception as e:
+                                print(f"Error querying: {e}")
+                                RETRY_COUNT -= 1
+                    if not response:
+                        raise Exception("LLM returned nothing even after retrying")
+                    print("response: ", response)
+                    relevant_items = self.process_response(response, self.item_id_map)
+                    root_item_name = ''
+                    items_added = 0
+                    max_limit = len(item_data['name'].split(' | '))
+                    for item in relevant_items:
+                        if items_added >= max_limit:
                             break
-                        except Exception as e:
-                            print(f"Error querying: {e}")
-                            RETRY_COUNT -= 1
-                if not response:
-                    raise Exception("LLM returned nothing even after retrying")
-                print("response: ", response)
-                relevant_items = self.process_response(response, self.item_id_map)
-                root_item_name = ''
-                items_added = 0
-                max_limit = len(item_data['name'].split(' | '))
-                for item in relevant_items:
-                    if items_added >= max_limit:
-                        break
-                    if items_added != 0:
-                        root_item_name += " | "
-                    root_item_name += item['name']
-                    items_added += 1
+                        if items_added != 0:
+                            root_item_name += " | "
+                        root_item_name += item['name']
+                        items_added += 1
 
-                print(f"Child Menu Name: {item_data['name']}\nRelevant Items:\n{json.dumps(relevant_items, indent=4)}\n")
-                if root_item_name != '':
-                    eval_prediction = Evaluator('gpt-4o-mini').item_evaluator(root_item_name, item_data['name'])
-                    eval_input = Evaluator('gpt-4o-mini').item_evaluator(data['mv_name'], item_data['name'])
-                    print(json.dumps(relevant_items[0], indent=4))
-                else:
-                    root_item_name = 'NOT FOUND'
-                    eval_input = Evaluator('gpt-4o-mini').item_evaluator(data['mv_name'], item_data['name'])
-                    print("None")
-            eval_input = bool(eval_input == 'YES')
-            eval_prediction = bool(eval_prediction == 'YES')
-            if root_item_name in ['AMBIGUOUS ITEM DETECTED', 'MRP ITEM DETECTED']:
-                eval_prediction = None
-            prediction = MenuMappingPrediction(menu_id=data['id'], menu_name=data['name'], master_menu_id=data['mv_id'], master_menu_name=data['mv_name'], corrected_menu_name=item_data['name'],
-                                               eval_current=eval_input, predicted_menu_name=root_item_name, eval_prediction=eval_prediction, response=str(response), ranked_nodes=ranked_nodes, log_id=log_id, quantitative_menu_name=item_data['quantity_details'])
-            prediction.save()
+                    print(f"Child Menu Name: {item_data['name']}\nRelevant Items:\n{json.dumps(relevant_items, indent=4)}\n")
+                    if root_item_name != '':
+                        eval_prediction = Evaluator('gpt-4o-mini').item_evaluator(root_item_name, item_data['name'])
+                        eval_input = Evaluator('gpt-4o-mini').item_evaluator(data['mv_name'], item_data['name'])
+                        print(json.dumps(relevant_items[0], indent=4))
+                    else:
+                        root_item_name = 'NOT FOUND'
+                        eval_input = Evaluator('gpt-4o-mini').item_evaluator(data['mv_name'], item_data['name'])
+                        print("None")
+                eval_input = bool(eval_input == 'YES')
+                eval_prediction = bool(eval_prediction == 'YES')
+                if root_item_name in ['AMBIGUOUS ITEM DETECTED', 'MRP ITEM DETECTED']:
+                    eval_prediction = None
+                prediction = MenuMappingPrediction(menu_id=data['id'], menu_name=data['name'], master_menu_id=data['mv_id'], master_menu_name=data['mv_name'], corrected_menu_name=item_data['name'],
+                                                eval_current=eval_input, predicted_menu_name=root_item_name, eval_prediction=eval_prediction, response=str(response), ranked_nodes=ranked_nodes, log_id=log_id, quantitative_menu_name=item_data['quantity_details'])
+                prediction.save()
+            except Exception as e:
+                print(f'Error: {str(e)}')
+                prediction = MenuMappingPrediction(menu_id=data['id'], menu_name=data['name'], master_menu_id=data['mv_id'], master_menu_name=data['mv_name'], corrected_menu_name=item_data.get('name'),
+                                                   eval_current=eval_input, predicted_menu_name=f'Error: {str(e)}', eval_prediction=eval_prediction, response=str(response), ranked_nodes=None, log_id=log_id, quantitative_menu_name=item_data.get('quantity_details'))
+                prediction.save()
             # ouput_text = f"""{data['id']},{data['name']},{item_data['name']},{data['mv_id']},{data['mv_name']},{eval_input},{root_item_name},{eval_prediction}\n"""
             # with open(file_path, "a") as file:
             #     file.write(ouput_text)
